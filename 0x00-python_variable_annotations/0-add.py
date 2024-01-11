3905ea0d9c2c88ffd2fde56eded3aa0ff80a25de
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-11 17:09:04 +0000

    add annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+"""
+    Adds two float numbers and returns the result.
+
+    Parameters:
+    - a (float): The first number.
+    - b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+"""
+
 def add(a: float, b: float) -> float:
 
     result = a + b
