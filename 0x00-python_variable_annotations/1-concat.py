7d71363a9898704e88b91a3048591458a0cbe419
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-12 15:21:59 +0000

    annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+i#!/usr/bin/env python3
+"""0. Basic annotations - add"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """0. Basic annotations - add"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""2. Basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """2. Basic annotations - add"""
+    return int(n)
