1261ebe864f6e7daa61438909b74aede46372182
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-15 17:19:07 +0000

    async function

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Curriculum
+Short Specializations
+Average: 118.3%
+0x01. Python - Async """
+
+import asyncio
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+import time
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    measure_time function
+    """
+    start: float = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end: float = time.time()
+    return (end - start) / n
