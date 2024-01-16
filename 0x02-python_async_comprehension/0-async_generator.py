ed997d2088fc639249518bcadc07ec3fd53288d8
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-16 22:45:16 +0000

    asnyc comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+!/usr/bin/env python3
+"""0x02. Python - Async Comprehension"""
+import asyncio
+from random import uniform
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Write a coroutine called async_generator that takes no arguments."""
+
+    for i in range(10):
+        yield uniform(0, 10)
+        await asyncio.sleep(1)
