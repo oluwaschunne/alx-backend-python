9c5256435fcde6b52f25324ca5388c5a52c641b0
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-17 11:46:13 +0000

    async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Write a coroutine called async_generator that takes no arguments."""
 
     for i in range(10):
