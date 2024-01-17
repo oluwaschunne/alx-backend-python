34448322d21be71ee0d67816d63efb32a6000a93
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-01-17 11:18:06 +0000

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,8 +8,4 @@ async_generator = __import__("0-async_generator").async_generator
 async def async_comprehension() -> List[float]:
     """Import async_generator from the previous task and then write
     a coroutine called async_comprehension that takes no arguments."""
-    arr = []
-    async for i in async_generator():
-        arr.append(i)
-
-    return arr
+    return [i async for i in async_generator()]
